3b6204c9f5a9cc9561511b888814e58c9b6516d8
James Alan Bush <jamesalanbush1972@gmail.com>
2021-05-01 23:10:15 -0400

    Initial Commit

---
+++ PlanetaryHourSwift/PlanetaryHourSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetaryHourSwiftApp.swift
+//  PlanetaryHourSwift
+//
+//  Created by Xcode Developer on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct PlanetaryHourSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
